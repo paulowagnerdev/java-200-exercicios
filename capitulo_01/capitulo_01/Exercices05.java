298735b9ec4a7059cfa87b90cfc4541f87f0f95d
paulowagnerdev <paulo.wagner@live.com>
2025-07-24 06:44:39 -0300

    Update 24/07

--- capitulo_01/capitulo_01/Exercices05.java
+++ capitulo_01/capitulo_01/Exercices05.java
@@ -3,8 +3,21 @@ package capitulo_01;
 public class Exercices05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/*
+		 * Exercício 5: Operações Aritméticas Desenvolva um programa que declare duas
+		 * variáveis int e realize as operações de soma, subtração, multiplicação,
+		 * divisão e módulo entre elas. Exiba os resultados de cada operação.
+		 */
+		
+		int a = 120;
+		int b = 50;
+		
+		System.out.println("Soma = " + (a + b));
+		System.out.println("Subtração = " + (a - b));
+		System.out.println("Multiplicação = " + a * b);
+		System.out.println("Divisão = " + a / b);
+		System.out.println("Módulo = " + a % b);
 	}
 
 }
